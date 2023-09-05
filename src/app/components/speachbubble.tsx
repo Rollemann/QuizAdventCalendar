a0d08d9b7fc0f384c635966439f89cc616702cae
Rolf Suslik <rolf.suslik@students.ffhs.ch>
2023-09-05 16:47:29 +0200

    aufgeräumt

--- src/app/components/speachbubble.tsx
+++ src/app/components/speachbubble.tsx
@@ -1,28 +1,28 @@
 import React from 'react'
 
-const speachbubble = ({mirror}:{mirror:boolean}) => {
-    
-    const pixelSize:number = 3;
-    const qube:string = `l ${pixelSize},0 l 0,${pixelSize}`;
-    const startX:number = 75;
-    const width:number = 300;
-    const height:number = 180;
-    const diagLength:number = 7;
+const speachbubble = ({ mirror }: { mirror: boolean }) => {
 
-    let diagLine:string = "";
-    for(let i:number = 0; i < diagLength; ++i){
-        const offset:number = i*pixelSize;
+    const pixelSize: number = 3;
+    const qube: string = `l ${pixelSize},0 l 0,${pixelSize}`;
+    const startX: number = 75;
+    const width: number = 300;
+    const height: number = 180;
+    const diagLength: number = 7;
+
+    let diagLine: string = "";
+    for (let i: number = 0; i < diagLength; ++i) {
+        const offset: number = i * pixelSize;
         diagLine += `M ${startX + offset},${offset} ${qube}`;
     }
 
     return (
         <svg viewBox={`0 0 ${width} ${height}`}>
             <path
-                transform={mirror? 'scale(-1,1) translate(-300,0)' : ""}
+                transform={mirror ? 'scale(-1,1) translate(-300,0)' : ""}
                 stroke='black'
                 strokeWidth={1}
                 fill='orange'
-                d={`${diagLine} M ${startX+diagLength*pixelSize},${diagLength*pixelSize} L ${width-10},${diagLength*pixelSize} L ${width-10},${height} L 10,${height} L 10,${diagLength*pixelSize} L ${startX},${diagLength*pixelSize} L ${startX},0`}
+                d={`${diagLine} M ${startX + diagLength * pixelSize},${diagLength * pixelSize} L ${width - 10},${diagLength * pixelSize} L ${width - 10},${height} L 10,${height} L 10,${diagLength * pixelSize} L ${startX},${diagLength * pixelSize} L ${startX},0`}
             />
             <text x={15} y={37}>
                 Placholder Text for later on.
