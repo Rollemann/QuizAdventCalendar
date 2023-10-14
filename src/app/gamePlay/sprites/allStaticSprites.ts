3bd6cda9d4587e8e5157a0ddf621b0158e5dd32b
Rollemann <suslikrolf@gmail.com>
2023-10-14 16:22:30 +0200

    move in statics rein

--- src/app/gamePlay/sprites/allStaticSprites.ts
+++ src/app/gamePlay/sprites/allStaticSprites.ts
@@ -10,103 +10,140 @@ export function allStaticSpritesProps(ctx: CanvasRenderingContext2D): StaticType
                     area: { x: 0, y: 710, width: ctx.canvas.width, height: 10 },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
                 },
                 {
                     area: { x: 275, y: 544, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 },
                 {
                     area: { x: 425, y: 544, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 },
                 {
                     area: { x: 575, y: 544, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 },
                 {
                     area: { x: 725, y: 544, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 },
                 {
                     area: { x: 875, y: 544, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 },
                 {
                     area: { x: 1025, y: 544, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 }, {
                     area: { x: 200, y: 378, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 }, {
                     area: { x: 350, y: 378, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 }, {
                     area: { x: 500, y: 378, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 }, {
                     area: { x: 650, y: 378, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 }, {
                     area: { x: 800, y: 378, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 }, {
                     area: { x: 950, y: 378, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 }, {
                     area: { x: 275, y: 212, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 }, {
                     area: { x: 425, y: 212, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 }, {
                     area: { x: 575, y: 212, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 }, {
                     area: { x: 725, y: 212, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 }, {
                     area: { x: 875, y: 212, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 }, {
                     area: { x: 1025, y: 212, width: mainWidth, height: mainHeight },
                     ctx: ctx,
                     imageSrc: './Questionmark.png',
-                    scale: 1
+                    scale: 1,
+                    moveProps: null
+
                 },
             ],
             walls: [
