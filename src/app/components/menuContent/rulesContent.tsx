2f9b28952066e92f03641c35e677a1ff21e452f2
Rolf Suslik <rolf.suslik@tn-ict.com>
2023-11-08 21:08:29 +0100

    DB tests

--- src/app/components/menuContent/rulesContent.tsx
+++ src/app/components/menuContent/rulesContent.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '@/app/firebase';
 
 async function getRules() {
-    /* const res = await fetch('http://localhost:8090/api/collections/rules/records?page=1', {cache:"no-cache"});
-    const data = await res.json();
-    return data.items; */
-    return [{text: "TODO"}]
+  /* const res = await fetch('http://localhost:8090/api/collections/rules/records?page=1', {cache:"no-cache"});
+  const data = await res.json();
+  return data.items; */
+  return [{ text: "TODO" }]
 }
 
 async function RulesContent() {
-    const rulesText = await getRules();
-    
+  const rulesText = await getRules();
+  const writeToDB = async () => {
+    await addDoc(collection(db, 'test'), {
+      test: 1,
+      test2: "hi"
+    });
+  }
+
   return (
-    <div>{rulesText[0].text}</div>
+    <>
+      <button onClick={writeToDB} className='bg-white w-max'>Add random stuff to DB</button>
+      <div>{rulesText[0].text}</div>
+    </>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { useEffect, useRef } from 'react'
 import { setup } from './gamePlay/setupGame';
 import { gameLoop } from './gamePlay/gameLoop';
 
-
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -13,13 +12,12 @@ export default function Home() {
       let setupResult = setup(canvas);
       if (setupResult) {
         let { ctx, sprites } = setupResult;
-        gameLoop(ctx, sprites, {Name: "", RoomTimes: []}); // TODO Second argument anpassen
+        gameLoop(ctx, sprites, { Name: "", RoomTimes: [] }); // TODO Second argument anpassen
       }
     }
   }, [])
 
-
   return (
-    <canvas ref={canvasRef} className='border-solid border-black border-2 bg-slate-300' />
+      <canvas ref={canvasRef} className='border-solid border-black border-2 bg-slate-300' />
   )
 }
